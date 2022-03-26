55bf9b192b3b2424eb43b507d3641f19299c1e30
Ahmet Deniz GÜNER <ahmetdenizguner@gmail.com>
2022-03-26 12:01:38 +0300

    unused code block removed

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"time"
 
 	"github.com/Picus-Security-Golang-Backend-Bootcamp/homework-3-AhmetDenizGuner/internal/database"
 	"github.com/Picus-Security-Golang-Backend-Bootcamp/homework-3-AhmetDenizGuner/internal/domain/author"
 	"github.com/Picus-Security-Golang-Backend-Bootcamp/homework-3-AhmetDenizGuner/internal/domain/book"
 	"github.com/Picus-Security-Golang-Backend-Bootcamp/homework-3-AhmetDenizGuner/internal/routers"
-	"github.com/Picus-Security-Golang-Backend-Bootcamp/homework-3-AhmetDenizGuner/pkg/csv_helper"
 )
 
 func init() {
@@ -74,67 +70,4 @@ func ShutdownServer(srv *http.Server, timeout time.Duration) {
 	// to finalize based on context cancellation.
 	log.Println("shutting down")
 	os.Exit(0)
-}
-
-func InitiliazeDatabase() error {
-
-	dbExist := database.DB.Migrator().HasTable(&book.Book{})
-
-	//chech DB is already exist
-	if !dbExist {
-		database.DB.AutoMigrate(&book.Book{})
-		database.DB.AutoMigrate(&author.Author{})
-
-		// read CSV
-		authorSlice, err := csv_helper.ReadCsv("../resources/author.csv", 1)
-
-		if err != nil {
-			fmt.Println(err)
-			return errors.New("CSV cannot be read!")
-		}
-
-		//create author data
-		var authorData []author.Author
-		for _, author2 := range authorSlice {
-
-			newAuthor := author.NewAuthor(author2[1])
-
-			authorData = append(authorData, *newAuthor)
-
-		}
-
-		// add author data
-		authorRepo := author.NewAuthorRepository(database.DB)
-		authorRepo.InsertInitialData(authorData)
-
-		//read book csv
-		bookSlice, err := csv_helper.ReadCsv("../resources/book.csv", 1)
-
-		if err != nil {
-			fmt.Println(err)
-			return errors.New("CSV cannot be read!")
-		}
-
-		//create book data
-		var bookData []book.Book
-
-		for _, book2 := range bookSlice {
-
-			price, _ := strconv.ParseFloat(book2[4], 64)
-			pageNum, _ := strconv.Atoi(book2[3])
-			stockQuantity, _ := strconv.Atoi(book2[5])
-			authorID, _ := strconv.Atoi(book2[6])
-			newBook := book.NewBook(pageNum, stockQuantity, price, book2[0], book2[2], book2[1], uint(authorID))
-
-			bookData = append(bookData, *newBook)
-
-		}
-
-		//insert book data
-		bookRepo := book.NewBookRepository(database.DB)
-		bookRepo.InsertInitialData(bookData)
-
-	}
-
-	return nil
 }
